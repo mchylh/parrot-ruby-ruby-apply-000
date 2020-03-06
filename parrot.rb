b3c063ccb3de2073739ec0f00c9015dd4c70097b
Michaylah Mam <mchylh@gmail.com>
2020-03-06 15:15:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot
-    puts "Squawk!"
+  puts "Squawk!"
   end
-    phrase = "Squawk!"
-    return phrase
+phrase = "Squawk!"
+  return phrase
   end
 end
 
