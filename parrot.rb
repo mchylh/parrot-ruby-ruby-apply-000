23bd47ea77770164ee3d530896fd57a5d0be7eb4
Michaylah Mam <mchylh@gmail.com>
2020-03-06 14:59:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -4,6 +4,16 @@ def parrot
   puts "Squawk!"
 end
 
-def parrot(phrase = "Squawk!")
+def parrot
+  phrase = "Squawk!"
+  return phrase
+end
+
+def parrot("Pretty bird!")
   puts parrot
+end
+
+def parrot
+  phrase = "Pretty bird!"
+  puts phrase
 end
