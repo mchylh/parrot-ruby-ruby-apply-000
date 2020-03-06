fc607bc23c7ad46e6b21cb2f0c4e1b9b02a2ae45
Michaylah Mam <mchylh@gmail.com>
2020-03-06 17:21:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,7 +2,9 @@
 # returns the phrase
 def parrot(phrase = "Squawk!")
   #should output "Squawk!" to the terminal when called without any arguments
+  #should output the given phrase when called with an argument for example; overrides "Squawks!" by calling parrot("Pretty bird!")
   puts "#{phrase}"
-  #should return the default phrase, "Squawk!" when called without any arguments' do
+  #should return the default phrase, "Squawk!" when called without any arguments
+  #should return the given phrase like "Pretty bird!" when called with an argument reassigning phrase = parrot("Pretty bird!")
   return phrase
 end
